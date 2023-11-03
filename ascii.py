4884a751599a8daf933db5d34ef18588dd4c4b74
Kokilavanisasi <148611945+Kokilavanisasi@users.noreply.github.com>
2023-11-03 10:26:48 +0530

    Add files via upload

---
+++ ascii.py
@@ -0,0 +1,165 @@
+'''a="python"
+
+
+z1="p"
+b1=ord(z1)
+c1=chr(b1-32)
+
+z2="y"
+b2=ord(z2)
+c2=chr(b2-32)
+
+z3="t"
+b3=ord(z3)
+c3=chr(b3-32)
+
+z4="h"
+b4=ord(z4)
+c4=chr(b4-32)
+
+z5="o"
+b5=ord(z5)
+c5=chr(b5-32)
+
+z6="n"
+b6=ord(z6)
+c6=chr(b6-32)
+
+print(c1+c2+c3+c4+c5+c6)
+
+
+a="PYTHON"
+
+
+z1="P"
+b1=ord(z1)
+c1=chr(b1+32)
+
+z2="Y"
+b2=ord(z2)
+c2=chr(b2+32)
+
+z3="T"
+b3=ord(z3)
+c3=chr(b3+32)
+
+z4="H"
+b4=ord(z4)
+c4=chr(b4+32)
+
+z5="O"
+b5=ord(z5)
+c5=chr(b5+32)
+
+z6="N"
+b6=ord(z6)
+c6=chr(b6+32)
+
+print(c1+c2+c3+c4+c5+c6)'''
+
+
+'''a="www.google.com"
+#b=a[1:]
+print(a[1:])
+
+a="www.google.com"
+b=a.replace("www.google.com","ww.google.com")
+print(b)'''
+
+
+#slice 8/9/23
+
+'''a="www.google.com"
+b=a.removeprefix("w")
+print(b)
+
+a="www.google.com.www"
+b=a.removesuffix("w")
+print(b)'''
+
+
+'''a="python program"
+print(a[0:5])
+
+a="python program"
+print(a[0:6])
+
+a="python program"
+print(a[7:14])
+
+a="python program"
+print(a[4:9])
+
+a="python program"
+print(a[-4:])
+
+a="python program"
+print(a[0:-1])
+
+a="python program"
+print(a[-1:0])'''
+
+'''a="python program"
+print(a[0:14:2])
+
+a="python program"
+print(a[-1:0:-2])'''
+
+'''a="python program"
+print(a[-1:0:2])'''
+
+'''a="AAAAA"
+b=(a[0:2])
+c=(a[2:3])
+d=(a[3:5])
+print(b,c,d)
+
+'''or'''
+a="AAAAA"
+print(a[0:2]+" "+a[2:3]+" "+a[3:5])'''
+
+
+
+
+
+'''a="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+print(a[-3:-2]+a[-6:-5]+a[-9:-8]+a[-12:-11]+a[-15:-14]+a[8:9]+a[5:6]+a[2:3])
+
+a="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+print(a[::-1])
+print(a[::1])
+
+a="ABCDE"
+print(a[0:8])'''
+
+
+'''a="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+print(a[-1:0:-3])
+print(a[1:0:-2])
+print(a[-5:-5])'''
+
+
+"""a="python program"
+b=len(a[7:14])
+print(b)
+
+
+a="ABCDEF|GHIJK|LMNOPQRST|UVWXYZ"
+b=a.split("|")
+print(len(b[3]))
+
+print(a[-5])
+
+
+
+
+a="python program"
+b=a.split()
+print(b[0]*3+" "+b[1])"""
+
+a="python program"
+b=a.split()
+c=b[0]*3+""+b[1]
+print(c.replace("h",""))
+
+     
